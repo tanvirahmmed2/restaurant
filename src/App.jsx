b8f8166c11935184310cd24c1f222c4578bc778e
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-27 14:03:03 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,10 @@ import WebDev from "./pages/WebDev";
 import WebDesign from "./pages/WebDesign";
 import Sidebar from "./layouts/Sidebar";
 import Cart from "./pages/Cart";
-import BG from './component/bgg.jpg'
 
 function App() {
   return (
-    <div className="w-full h-auto relative overflow-x-hidden text-white bg-sk">
+    <div className="w-full h-auto relative overflow-x-hidden bg-indigo-900 text-white bg-sk">
       <Navbar />
       <Sidebar />
       <div className="w-full h-auto mt-20 min-h-screen flex flex-col items-center justify-center">
@@ -42,7 +41,6 @@ function App() {
         </Routes>
       </div>
       <Footer />
-      <img src={BG} alt="background" className="w-full h-screen fixed object-cover top-0 blur-sm -z-20" />
     </div>
   );
 }
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
 
   const {sidebar,setSidebar}= useContext(CreateContext)
   return (
-    <section className='w-full h-auto fixed bg-black/10 text-white backdrop-blur-lg shadow-2xl top-0 z-40'>
+    <section className='w-full h-auto fixed bg-indigo-950 text-white backdrop-blur-lg shadow-2xl top-0 z-40'>
 
-      <nav className='w-full h-16 backdrop-blur-lg flex flex-row items-center justify-between lg:justify-around px-6'>
+      <nav className='w-full h-16 backdrop-blur-lg flex flex-row items-center justify-between px-6'>
         <a href="/" className='text-3xl font-semibold'>Disibin</a>
 
         <div className='w-auto hidden md:flex flex-row h-16 items-center justify-center  font-semibold'>
@@ -23,7 +23,7 @@ const Navbar = () => {
           <Link className='h-16 flex items-center justify-center hover:border-b-2 px-4' to="/cart">Cart</Link>
           <Link className='h-16 flex items-center justify-center hover:border-b-2 px-4' to="/contact">Contact</Link>
         </div>
-        <div  className='h-16 cursor-pointer flex  items-center justify-center flex-row gap-4 px-6'>
+        <div  className='h-16 cursor-pointer flex  items-center justify-center flex-row gap-4 '>
           
           <Link className='h-7 flex items-center justify-center px-4 bg-gradient-to-br from-cyan-500 to-emerald-500 rounded-lg ' to="/signup">SignUp</Link>
 
